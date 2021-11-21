d9b0c4daba28ed3117c18dae10e13afdfeb32e1c
Cassel-Evens <casselke@gmail.com>
2021-11-20 23:48:24 -0800

    add first java test and base html layout

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Buisness Logic 
+
+/* Run a for.Each loop to run through the index of an array until it 
+until it reaches user imputted value. Then with if statements it will
+match corresponding variables to strings and display those in an array
+output */
+
+const text = ("9");
+let number = parseInt(text);
+console.log(number);
+for (let i = 1; i <= number; i +=1) {
+  console.log(i);
+}  
+// function numberReplacer(text) {
+//   let numbersArray = [0,1,2,3,4,5];
+//   numbersArray.forEach(function(number) {
+//     if (text.match(number)) {
+      
+//     }
+//   })
+// }
