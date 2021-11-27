ff11c38d3c30427d3a3f7d058b0f5a3987e8b823
Cassel-Evens <casselke@gmail.com>
2021-11-27 15:24:03 -0800

    add changes to html and css as well as UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
 // Buisness Logic 
-
-/* Run a for.Each/For loop to run through the index of an array until it 
-until it reaches user imputted value. Then with if statements it will
-match corresponding variables to strings and display those in an array
-output */
 let robogers = function(number){
   if (isNaN(number)) {
     return ("I do not believe this is a number my dear neighbor!")
@@ -15,17 +10,40 @@ let robogers = function(number){
     let robogersArrayMod = robogersArray.map(function(num) {
       let digit = num.toString().split("");
       if (digit.includes("1")) {
-        return "\"Let's be neighbors!";
+        return "\Let's be neighbors!";
       } else if (digit.includes("2")) {
-        return "\"Boop";
+        return "\Boop";
       } else if (digit.includes("3")) {
-        return "\"Beep";
+        return "\Beep";
+      } else if (digit.includes("8")) {
+        return "\Join the neighborhood";
+      } else if (digit.includes("9")) {
+        return "\Wow you know some high numbers!";
+      } else if (digit.includes("4")) {
+        return "\This is my fave number";
       } else {
         return num;
       }
     });
-    console.log(robogersArray);
-    return robogersArrayMod.join(" ");
+    return robogersArrayMod.join(", ");
   };
 };
 
+//UI Logic
+$(document).ready(function() {
+  $("form#your-number").submit(function(event) {
+    event.preventDefault();
+    let number = parseInt($("input#Number")).val();
+    let robogerOutput = robogers(number);
+    $("#your-number-phrase").text(robogerOutput);
+  });
+});
+// $(document).ready(function() {
+//   $("form#mr-robogers").submit(function(event) {
+//     event.preventDefault();
+//     var number = parseInt($("input#input").val());
+//     var robogerOutput = robogers(number);
+//     $("#output").text(robogerOutput);
+//     $("#robogers").fadeIn();
+//   });
+// });
